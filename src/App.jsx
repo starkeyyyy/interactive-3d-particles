ed60e43ddaab5c78fbfc8378bdef6744ad102c9c
starkey <tanishqgupta013@gmail.com>
2025-05-10 15:08:08 +0530

    minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 import './App.css'
-import Interaction from "./components/testing";
+import Interaction from "./components/particle";
 
 function App() {
   const [count, setCount] = useState(0)
